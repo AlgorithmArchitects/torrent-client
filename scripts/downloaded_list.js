9da80871c5f7683c32bbb3f656c8fef9ee371e13
AndrewDorn <dornadigital@gmail.com>
2016-12-01 11:44:14 -0600

    Built some functions to aid in file previews.

--- scripts/downloaded_list.js
+++ scripts/downloaded_list.js
@@ -1,11 +1,12 @@
 var items = [];
 var count = 0;
-function AddToList(name, status, preview)
+function AddToList(name, status)
 {
 	var item = {
 		name: name,
 		status: status,
-		preview: preview
+		preview: "<img id=\"" + count + "\" src=\"#\" alt=\"\" />",
+		index: count
 	};
 	
 	items.push(item);
@@ -16,18 +17,33 @@ function AddToList(name, status, preview)
 
 function RefreshList()
 {
-	document.getElementById("table_body").innerHTML = "";
+	var list = ""
 	for(var i = 0; i < count; i++)
 	{
-		document.getElementById("table_body").innerHTML += BuildHtmlString(items[i]);
+		list += BuildHtmlString(items[i]);
 	}
+	document.getElementById("table_body").innerHTML = list;
 }
 
 function BuildHtmlString(item)
 {
 	if(item.status == "Failed")
-		return "<tr><td>" + items[i].name + "</td><td><span class=\"label label-danger\">" + items[i].status + "</span></td><td>" + items[i].preview + "</td></tr>\n";
+		return "<tr><td>" + item.name + "</td><td><span class=\"label label-danger\">" + item.status + "</span></td><td>" + item.preview + "</td></tr>\n";
 	if(item.status == "Success")
-		return "<tr><td>" + items[i].name + "</td><td><span class=\"label label-success\">" + items[i].status + "</span></td><td>" + items[i].preview + "</td></tr>\n";
-	return "<tr><td>" + items[i].name + "</td><td><span class=\"label label-info\">" + items[i].status + "</span></td><td>" + items[i].preview + "</td></tr>\n";
+		return "<tr><td>" + item.name + "</td><td><span class=\"label label-success\">" + item.status + "</span></td><td>" + item.preview + "</td></tr>\n";
+	return "<tr><td>" + item.name + "</td><td><span class=\"label label-info\">" + item.status + "</span></td><td>" + item.preview + "</td></tr>\n";
+}
+
+function readImage(file, index) {
+	var reader = new FileReader();
+
+	reader.onload = function (e) {
+		$("#" + index)
+			.attr('src', e.target.result)
+			.width(150)
+			.height(200);
+		//Need to update items
+	};
+
+	reader.readAsDataURL(file);
 }
